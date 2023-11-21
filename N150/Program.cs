b09757b77f4d3827769534671a788f8547fc9d0a
Rasmus <Rpv77@THEBEAST>
2023-11-21 13:47:48 +0100

    Tilføjet N150

---
+++ N150/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace N150
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //På typen
+            bool findesFil = System.IO.File.Exists(@"c:\temp\test.txt");    
+            Console.Write("Findes fil:\n");
+            if (findesFil)
+            {
+                Console.WriteLine("Ja");
+            }
+            else Console.WriteLine("Nej");
+
+            Console.WriteLine($"\nIndhold af fil:");
+            string indhold = System.IO.File.ReadAllText(@"c:\temp\test.txt");
+            Console.WriteLine(indhold);
+
+            // På instance
+            var fil = new System.IO.FileInfo(@"c:\temp\test.txt");
+            DateTime d = fil.LastAccessTime;
+            Console.WriteLine($"Fil sidst åbnet: \n{d}");
+            string ex = fil.Extension;
+            Console.WriteLine($"\nFilens extension: \n{ex}");
+        }
+    }
+}
