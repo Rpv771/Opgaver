0487cf86a0f2c64fdced0eb08685c8230a8f79d5
Rasmus <Rpv77@THEBEAST>
2023-11-28 11:23:38 +0100

    Tilføjet N239 + N250

---
+++ N239/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+namespace N239
+{
+   internal class Program
+   {
+        static void Main(string[] args)
+        {
+        string json = System.IO.File.ReadAllText(@"c:\temp\test2.txt");
+        List<Person> list = System.Text.Json.JsonSerializer.Deserialize<List<Person>>(json);
+
+        foreach (var item in list)
+        {
+            Console.WriteLine("Id: " + item.Id);
+            Console.WriteLine("Navn: " + item.Navn);
+            Console.WriteLine("Er Dansk: " + item.ErDansk);
+            Console.WriteLine("Dato: " + item.Dato+"\n");
+        }
+
+
+
+        }
+   }
+
+   public class Person
+   {
+   public int Id { get; set; }
+   public string Navn { get; set; }
+   public bool ErDansk { get; set; }
+   public DateTime Dato { get; set; }
+   }
+   
+}
---
+++ N250/Program.cs
@@ -0,0 +1,79 @@
+﻿namespace N250
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Terning t = new Terning();
+            //t.Skriv();
+            //t.Ryst();
+            //t.Skriv();
+
+            LudoTerning l = new LudoTerning();
+            l.Skriv();
+            Console.WriteLine(l.ErGlobus());
+            Console.WriteLine(l.ErStjerne());
+        }
+    }
+    public class Terning
+    {
+        private static System.Random rnd;
+        private int værdi;
+
+        public int Værdi
+        {
+            get
+            {
+                Console.WriteLine("Terning aflæses som " + this.værdi);
+                return this.værdi;
+            }
+            set
+            {
+                if (value < 0 || value > 6)
+                    value = 1;
+                Console.WriteLine("Terning tildeles " + value);
+                this.værdi = value;
+            }
+        }
+
+        public void Skriv()
+        {
+            Console.WriteLine("[" + this.Værdi + "]");
+        }
+
+        public void Ryst()
+        {
+            this.Værdi = rnd.Next(1, 7);
+        }
+
+        public Terning()
+        {
+            this.Ryst();
+        }
+        public Terning(int værdi)
+        {
+            this.Værdi = værdi;
+        }
+        static Terning()
+        {
+            rnd = new Random();
+        }
+    }
+
+    public sealed class LudoTerning : Terning
+    {
+        public bool ErGlobus()
+        {
+            return this.Værdi == 3;
+        }
+        public bool ErStjerne()
+        {
+            return this.Værdi == 5;
+        }
+        public LudoTerning() : base()
+        { }
+
+        public LudoTerning(int værdi) : base(værdi)
+        { }
+    }
+}
